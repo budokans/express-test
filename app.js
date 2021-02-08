59c69195e20e920e28e3f304aef25ab0383dc021
budokans <68995729+budokans@users.noreply.github.com>
2021-02-09 00:42:26 +1300

    Exporting by adding methods to the exports object

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 2073;
+const { area, perimeter } = require("./square.js");
 
 // Route definition: the callback function executes when a GET request is made to the path in the first argument.
 app.get("/", (req, res) => {
@@ -11,3 +12,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+console.log(`The area of a square with length 7 is ${area(7)}`);
---
+++ square.js
@@ -0,0 +1,2 @@
+exports.area = (width) => width * width;
+exports.perimeter = (width) => width * 4;
